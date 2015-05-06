bfbbf659f5397d0246d36bb3b75073d85633349f
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2015-05-06 09:05:22 +0100

    First cut version: one message, all pages, languages at time of install

---
+++ catalog/includes/languages/english/modules/content/header/cm_header_store_message.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2015 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_STORE_MESSAGE_TITLE', 'Store Message');
+  define('MODULE_CONTENT_STORE_MESSAGE_DESCRIPTION', 'Display a message on every page of the store');
+?>
---
+++ catalog/includes/modules/content/header/templates/storemessage.php
@@ -0,0 +1,8 @@
+<style type='text/css'> 
+	div.storemessage-error { background-color:#FF9999; } 
+	div.storemessage-warning { background-color:#FFFF99; } 
+	div.storemessage-info { background-color:#9999FF; } 
+</style>
+<div class="col-sm-12 text-center storemessage-<?php echo MODULE_CONTENT_STORE_MESSAGE_TYPE; ?>">
+  <?php echo $store_msg; ?>
+</div>
